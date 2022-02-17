e2aae45a69502557f4ace2700c0bb1e9abf1ef39
BenjaminRodot <78915615+BenjaminRodot@users.noreply.github.com>
2022-02-17 16:10:25 +0100

    Create Perso.vue

---
+++ src/components/Perso.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <PersoCaracs :currentPlayer="currentPlayer"></PersoCaracs>
+    <PersoSlots :currentPlayer="currentPlayer"></PersoSlots>
+    <PersoOps :currentPlayer="currentPlayer" :currentShop="currentShop" :currentTown="currentTown"></PersoOps>
+  </div>
+</template>
+
+<script>
+import PersoCaracs from '@/components/PersoCaracs'
+import PersoSlots from '@/components/PersoSlots'
+import PersoOps from '@/components/PersoOps'
+export default {
+  name: "Perso",
+  components: {
+    'PersoCaracs': PersoCaracs,
+    'PersoSlots' : PersoSlots,
+    'PersoOps' : PersoOps,
+  },
+  props:{
+    currentPlayer:Object,
+    currentShop:Object,
+    currentTown:Object,
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
