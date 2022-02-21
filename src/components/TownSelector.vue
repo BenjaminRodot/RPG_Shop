94ffa1c75c823be5576e9ee766778d99833ab65b
BenjaminRodot <b.a.rodot@orange.fr>
2022-02-20 23:18:36 -0500

    3 area placées sur les nom des pays

--- src/components/TownSelector.vue
+++ src/components/TownSelector.vue
@@ -2,8 +2,12 @@
   <div>
     <img src="../image/map-world.png" alt="World Map" usemap="#map-world">
     <map name="map-world">
-      <area shape="rect" coords="15, 15, 200, 200"
+      <area shape="rect" coords="130, 105, 315, 140"
             href="https://www.google.fr" alt="exemple de rectangle" />
+      <area shape="rect" coords="140, 165, 315, 210"
+            href="https://www.youtube.com" alt="exemple de rectangle" />
+      <area shape="rect" coords="400, 115, 480, 150"
+            href="https://www.youtube.com/watch?v=5dkFBE49Zho" alt="exemple de rectangle" />
     </map>
     <div style="width: 50%">
       <div v-for="(town,index) in townsList" :key="index" @change="selectTown(index)">
